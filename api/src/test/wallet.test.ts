4f5fe087b4fc1544a6a12b3e08c4578e64e51b38
Melina Lazzaro <melinalazzaro01@gmail.com>
2022-10-25 18:41:35 -0300

    Agregamos parseo de valores

--- api/src/test/wallet.test.ts
+++ api/src/test/wallet.test.ts
@@ -13,11 +13,24 @@ class Wallet {
     }
 
     buyAmount(amount: number) {
+        if (amount < 0) {
+            throw new Error("Amount must be positive");
+        }
         this.balance += amount;
     }
 
     sellAmount(amount: number) {
+        if (amount < 0) {
+            throw new Error("Amount must be positive");
+        }
+        if (amount > this.balance) {
+            throw new Error("Amount must be lower than balance");
+        }
         this.balance -= amount;
+    }
+
+    getBalance() {
+        return this.balance;
     }
 }
 
@@ -44,4 +57,52 @@ describe('wallet', function () {
         wallet.sellAmount(1);   
         assert.deepEqual(wallet.isEmpty(), true);
     });
+
+    it('should not be empty after initialize it and add amount of currency and remove the less amount', function(){
+        const wallet = new Wallet();
+        wallet.buyAmount(2);   
+        wallet.sellAmount(1);   
+        assert.deepEqual(wallet.isEmpty(), false);
+    });
+
+    it('should be zero amount after initialize it', function(){
+        const wallet = new Wallet();        
+        assert.deepEqual(wallet.getBalance(), 0);
+    });
+
+    it('should have 1 amount after initialize it and add 2 amount of currency and remove 1 amount', function(){
+        const wallet = new Wallet();
+        wallet.buyAmount(2);   
+        wallet.sellAmount(1);   
+        assert.deepEqual(wallet.getBalance(), 1);
+    });
+
+    it('should not be able to add negative amount', function() {
+        const wallet = new Wallet();
+        assert.throws(
+            () => wallet.buyAmount(-1),
+            Error
+        );
+        assert.deepEqual(wallet.getBalance(), 0);
+    });
+
+    it('should not be able to sell negative amount', function() {
+        const wallet = new Wallet();
+        assert.throws(
+            () => wallet.sellAmount(-1),
+            Error
+        );
+        assert.deepEqual(wallet.getBalance(), 0);
+    });
+
+    it('should not be able to sell more than current balance', function() {
+        const wallet = new Wallet();
+        assert.throws(
+            () => wallet.sellAmount(1),
+            Error
+        );
+        assert.deepEqual(wallet.getBalance(), 0);
+        
+    });
+    
 })
