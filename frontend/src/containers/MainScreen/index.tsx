c6b0f177e3f0c56bccdc0699a9a8f4d8a765fe2a
Agustina Bocaccio <73569719+agusbocaccio@users.noreply.github.com>
2022-11-13 12:11:08 -0300

    added navbar to main screen

--- frontend/src/containers/MainScreen/index.tsx
+++ frontend/src/containers/MainScreen/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavContainer } from "./styled";
 
 export const MainScreen = () => {
   return (
-    <div>
-      <h1>Main Screen</h1>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <NavContainer>
+        <Navbar.Brand href="#home">CryptoMonitor</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#wallet" >Wallet</Nav.Link>
+          <Nav.Link href="#rules">Rules</Nav.Link>
+        </Nav>
+      </NavContainer>
+    </Navbar>
   );
 };
+
+export default MainScreen;
