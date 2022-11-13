21a758be6805e4250b285d995891d15fb95f4342
Agustina Bocaccio <73569719+agusbocaccio@users.noreply.github.com>
2022-11-13 12:11:48 -0300

    added login elements

--- frontend/src/containers/Register/index.tsx
+++ frontend/src/containers/Register/index.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { Container, NavLink } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const Register = () => {
   return (
-    <div>
-      <button>aaa</button>
-    </div>
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="dark" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <NavLink>Go back to main screen</NavLink>
+    </Container>
   );
 };
+
+export default Register;
